56d206ed0a869acb30d26efbf5fd19bcfbceb456
kate <kate.khomra@gmail.com>
2024-04-05 02:32:35 +0300

    lab1 parking

--- agent/src/domain/aggregated_data.py
+++ agent/src/domain/aggregated_data.py
@@ -3,10 +3,12 @@ from datetime import datetime
 
 from domain.accelerometer import Accelerometer
 from domain.gps import Gps
+from domain.parking import Parking
 
 
 @dataclass
 class AggregatedData:
     accelerometer: Accelerometer
     gps: Gps
+    parking: Parking
     time: datetime
--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -2,21 +2,24 @@ import random
 from csv import reader
 from datetime import datetime
 
+from domain.parking import Parking
 from domain.gps import Gps
 from domain.accelerometer import Accelerometer
 from domain.aggregated_data import AggregatedData
 
 
 class FileDatasource:
-    def __init__(self, accelerometer_filename: str, gps_filename: str) -> None:
+    def __init__(self, accelerometer_filename: str, gps_filename: str, parking_filename: str) -> None:
         self.accelerometer_filename = accelerometer_filename
         self.gps_filename = gps_filename
+        self.parking_filename = parking_filename
         self.accelerometer_data = []
         self.gps_data = []
+        self.parking_data = []
         self.last_read_index = 0
 
     def read(self) -> AggregatedData:
-        if not self.accelerometer_data or not self.gps_data:
+        if not self.accelerometer_data or not self.gps_data or not self.parking_data:
             print("No data available to read.")
             return None
 
@@ -34,11 +37,13 @@ class FileDatasource:
         while start_index < len(self.accelerometer_data):
             accelerometer_rows = self.accelerometer_data[start_index:end_index]
             gps_rows = self.gps_data[start_index:end_index]
+            parking_rows = self.parking_data[start_index:end_index]
 
-            for accel_row, gps_row in zip(accelerometer_rows, gps_rows):
+            for accel_row, gps_row, parking_row in zip(accelerometer_rows, gps_rows, parking_rows):
                 accelerometer = Accelerometer(*accel_row)
                 gps = Gps(*gps_row)
-                aggregated_data.append(AggregatedData(accelerometer, gps, datetime.now()))
+                parking = Parking(parking_row[0], Gps(parking_row[1], parking_row[2]))
+                aggregated_data.append(AggregatedData(accelerometer, gps, parking, datetime.now()))
 
             start_index = end_index
             end_index = min(start_index + batch_size, len(self.accelerometer_data))
@@ -49,6 +54,7 @@ class FileDatasource:
             else:
                 self.last_read_index = end_index
 
+        print()
         print("Data reading completed.")
         return aggregated_data
 
@@ -56,6 +62,7 @@ class FileDatasource:
         print("Starting to read files...")
         self.accelerometer_data = self.read_csv(self.accelerometer_filename)
         self.gps_data = self.read_csv(self.gps_filename)
+        self.parking_data = self.read_csv(self.parking_filename)
         print("Reading started")
 
     def stop_reading(self):
@@ -63,6 +70,7 @@ class FileDatasource:
         print("Reading stopped")
         self.accelerometer_data = []
         self.gps_data = []
+        self.parking_data = []
         self.last_read_index = 0
 
     def read_csv(self, filename: str):
@@ -73,5 +81,6 @@ class FileDatasource:
             next(csv_reader)
             for row in csv_reader:
                 data.append([float(cell) for cell in row])
+        print(data)
         print(f"Finished reading CSV file: {filename}")
         return data
